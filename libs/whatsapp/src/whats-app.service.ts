7a3546650fa1ddae548609db53d8df0c8d7d9d01
Yovangga Anandhika <dka.tech.dev@gmail.com>
2024-10-08 19:49:50 +0800

    adding doc

--- libs/whatsapp/src/whats-app.service.ts
+++ libs/whatsapp/src/whats-app.service.ts
@@ -1,5 +1,5 @@
 import WAWebJS, { Client, RemoteAuth, Store } from 'whatsapp-web.js';
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, Logger } from '@nestjs/common';
 import MongoStore from '@dkanest/mongostore';
 import { Connection } from 'mongoose';
 import { InjectConnection } from '@nestjs/mongoose';
@@ -7,6 +7,14 @@ import * as path from 'node:path';
 import TypedEmitter from 'typed-emitter';
 import { WhatsAppEvents } from './whats-app.type';
 import { EventEmitter } from 'events';
+import * as process from 'node:process';
+import * as fs from 'node:fs';
+import {
+  BrowserConnectOptions,
+  BrowserLaunchArgumentOptions,
+  ConnectOptions,
+  LaunchOptions,
+} from 'puppeteer';
 
 @Injectable()
 export class WhatsAppService implements OnModuleInit {
@@ -38,13 +46,47 @@ export class WhatsAppService implements OnModuleInit {
 
   private _register = new EventEmitter() as TypedEmitter<WhatsAppEvents>;
 
+  private logger = new Logger(WhatsAppService.name);
   constructor(@InjectConnection() connection: Connection) {
     /** get store mongo DB **/
     this.store = new MongoStore({ connection: connection });
+
+    const puppeteerConfig: BrowserLaunchArgumentOptions &
+      LaunchOptions &
+      BrowserConnectOptions &
+      ConnectOptions = {
+      args: [
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--disable-setuid-sandbox',
+        '--no-first-run',
+        '--no-sandbox',
+        '--no-zygote',
+        '--deterministic-fetch',
+        '--disable-features=IsolateOrigins',
+        '--disable-site-isolation-trials',
+        // '--single-process',
+      ],
+      //args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    };
+
+    if (fs.existsSync('/usr/bin/chromium-browser')) {
+      puppeteerConfig.executablePath = '/usr/bin/chromium-browser';
+    }
+
+    if (fs.existsSync(process.env.CHROME_BIN)) {
+      puppeteerConfig.executablePath = process.env.CHROME_BIN;
+    }
+
+    if (
+      process.env.PUPPETEER_HEADLESS !== undefined &&
+      process.env.PUPPETEER_HEADLESS === 'true'
+    ) {
+      puppeteerConfig.headless = true;
+    }
+
     this.client = new Client({
-      puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      },
+      puppeteer: puppeteerConfig,
       webVersionCache: {
         type: 'local',
         path: path.join(process.cwd(), './.whatsapp/cache'),
@@ -67,6 +109,13 @@ export class WhatsAppService implements OnModuleInit {
     this.client.on('message', (data) => {
       this.register.emit('message', data);
     });
-    await this.client.initialize();
+    await this.client
+      .initialize()
+      .then((res) => {
+        this.logger.log(res);
+      })
+      .catch((error) => {
+        this.logger.error(error);
+      });
   }
 }
